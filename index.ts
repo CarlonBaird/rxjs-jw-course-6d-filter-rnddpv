3810a84c681ab0a0ba4e126596287f6dd059a82d
CarlonBaird <carlyestrella@live.com>
2022-11-22 12:01:15 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,29 @@
+import { Observable } from 'rxjs';
 
+interface NewsItem {
+  //our model
+  category: 'Business' | 'Sports';
+  content: string;
+}
 
+const newsFeed$ = new Observable<NewsItem>((subscriber) => {
+  setTimeout(() => {
+    subscriber.next({ category: 'Business', content: 'A' });
+  }, 1000);
+
+  setTimeout(() => {
+    subscriber.next({ category: 'Sports', content: 'B' });
+  }, 3000);
+
+  setTimeout(() => {
+    subscriber.next({ category: 'Business', content: 'C' });
+  }, 4000);
+
+  setTimeout(() => {
+    subscriber.next({ category: 'Sports', content: 'D' });
+  }, 6000);
+
+  setTimeout(() => {
+    subscriber.next({ category: 'Sports', content: 'E' });
+  }, 7000);
+});
